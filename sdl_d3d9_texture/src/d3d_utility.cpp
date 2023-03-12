9c7c59b75ffe1df969c9714ddd686b5cd3543539
Aleksey Komarov <q4arus@ya.ru>
2023-03-13 00:34:49 +0300

    add src for sdl_d3d9_texture

---
+++ sdl_d3d9_texture/src/d3d_utility.cpp
@@ -0,0 +1,191 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// File: d3dUtility.cpp
+//
+// Author: Frank Luna (C) All Rights Reserved
+//
+// System: AMD Athlon 1800+ XP, 512 DDR, Geforce 3, Windows XP, MSVC++ 7.0
+//
+// Desc: Provides utility functions for simplifying common tasks.
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "d3d_utility.h"
+
+#include <SDL2/SDL_syswm.h>
+#ifdef USE_NINE
+#include "nine_sdl.h"
+#endif
+
+void* d3d::OSHandle(SDL_Window* Window)
+{
+	if (!Window)
+		return nullptr;
+
+#ifdef _WIN32
+	SDL_SysWMinfo info;
+	SDL_VERSION(&info.version);
+	SDL_GetWindowWMInfo(Window, &info);
+	return info.info.win.window;
+#else
+	return Window;
+#endif
+}
+
+bool d3d::InitD3D(
+	SDL_Window* Window,
+	int width, int height,
+	bool windowed,
+	D3DDEVTYPE deviceType,
+	IDirect3DDevice9** device)
+{
+	// Init D3D:
+
+	HRESULT hr = 0;
+	HWND hwnd = static_cast<HWND>(d3d::OSHandle(Window));
+
+	// Step 1: Create the IDirect3D9 object.
+
+	IDirect3D9* d3d9 = 0;
+#ifdef USE_NINE
+	d3d9 = Direct3DCreate9_SDL(Window);
+#else
+	d3d9 = Direct3DCreate9(D3D_SDK_VERSION);
+#endif
+
+	if( !d3d9 )
+	{
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Direct3DCreate9() - FAILED", nullptr);
+		return false;
+	}
+
+	// Step 2: Check for hardware vp.
+
+	D3DCAPS9 caps;
+	d3d9->GetDeviceCaps(D3DADAPTER_DEFAULT, deviceType, &caps);
+
+	int vp = 0;
+	if( caps.DevCaps & D3DDEVCAPS_HWTRANSFORMANDLIGHT )
+		vp = D3DCREATE_HARDWARE_VERTEXPROCESSING;
+	else
+		vp = D3DCREATE_SOFTWARE_VERTEXPROCESSING;
+
+	// Step 3: Fill out the D3DPRESENT_PARAMETERS structure.
+
+	D3DPRESENT_PARAMETERS d3dpp;
+	d3dpp.BackBufferWidth            = width;
+	d3dpp.BackBufferHeight           = height;
+	d3dpp.BackBufferFormat           = D3DFMT_A8R8G8B8;
+	d3dpp.BackBufferCount            = 1;
+	d3dpp.MultiSampleType            = D3DMULTISAMPLE_NONE;
+	d3dpp.MultiSampleQuality         = 0;
+	d3dpp.SwapEffect                 = D3DSWAPEFFECT_DISCARD;
+	d3dpp.hDeviceWindow              = hwnd;
+	d3dpp.Windowed                   = windowed;
+	d3dpp.EnableAutoDepthStencil     = true;
+	d3dpp.AutoDepthStencilFormat     = D3DFMT_D24S8;
+	d3dpp.Flags                      = 0;
+	d3dpp.FullScreen_RefreshRateInHz = D3DPRESENT_RATE_DEFAULT;
+	d3dpp.PresentationInterval       = D3DPRESENT_INTERVAL_IMMEDIATE;
+
+	// Step 4: Create the device.
+
+	hr = d3d9->CreateDevice(
+		D3DADAPTER_DEFAULT, // primary adapter
+		deviceType,         // device type
+		hwnd,               // window associated with device
+		vp,                 // vertex processing
+		&d3dpp,             // present parameters
+		device);            // return created device
+
+	if( FAILED(hr) )
+	{
+		// try again using a 16-bit depth buffer
+		d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
+
+		hr = d3d9->CreateDevice(
+			D3DADAPTER_DEFAULT,
+			deviceType,
+			hwnd,
+			vp,
+			&d3dpp,
+			device);
+
+		if( FAILED(hr) )
+		{
+			d3d9->Release(); // done with d3d9 object
+			SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "CreateDevice() - FAILED", nullptr);
+			return false;
+		}
+	}
+
+	d3d9->Release(); // done with d3d9 object
+
+	return true;
+}
+
+D3DLIGHT9 d3d::InitDirectionalLight(D3DXVECTOR3* direction, D3DXCOLOR* color)
+{
+	D3DLIGHT9 light;
+	::ZeroMemory(&light, sizeof(light));
+
+	light.Type      = D3DLIGHT_DIRECTIONAL;
+	light.Ambient   = *color * 0.4f;
+	light.Diffuse   = *color;
+	light.Specular  = *color * 0.6f;
+	light.Direction = *direction;
+
+	return light;
+}
+
+D3DLIGHT9 d3d::InitPointLight(D3DXVECTOR3* position, D3DXCOLOR* color)
+{
+	D3DLIGHT9 light;
+	::ZeroMemory(&light, sizeof(light));
+
+	light.Type      = D3DLIGHT_POINT;
+	light.Ambient   = *color * 0.4f;
+	light.Diffuse   = *color;
+	light.Specular  = *color * 0.6f;
+	light.Position  = *position;
+	light.Range        = 1000.0f;
+	light.Falloff      = 1.0f;
+	light.Attenuation0 = 1.0f;
+	light.Attenuation1 = 0.0f;
+	light.Attenuation2 = 0.0f;
+
+	return light;
+}
+
+D3DLIGHT9 d3d::InitSpotLight(D3DXVECTOR3* position, D3DXVECTOR3* direction, D3DXCOLOR* color)
+{
+	D3DLIGHT9 light;
+	::ZeroMemory(&light, sizeof(light));
+
+	light.Type      = D3DLIGHT_SPOT;
+	light.Ambient   = *color * 0.4f;
+	light.Diffuse   = *color;
+	light.Specular  = *color * 0.6f;
+	light.Position  = *position;
+	light.Direction = *direction;
+	light.Range        = 1000.0f;
+	light.Falloff      = 1.0f;
+	light.Attenuation0 = 1.0f;
+	light.Attenuation1 = 0.0f;
+	light.Attenuation2 = 0.0f;
+	light.Theta        = 0.5f;
+	light.Phi          = 0.7f;
+
+	return light;
+}
+
+D3DMATERIAL9 d3d::InitMtrl(D3DXCOLOR a, D3DXCOLOR d, D3DXCOLOR s, D3DXCOLOR e, float p)
+{
+	D3DMATERIAL9 mtrl;
+	mtrl.Ambient  = a;
+	mtrl.Diffuse  = d;
+	mtrl.Specular = s;
+	mtrl.Emissive = e;
+	mtrl.Power    = p;
+	return mtrl;
+}
---
+++ sdl_d3d9_texture/src/sdl_d3d9_texture.cpp
@@ -0,0 +1,234 @@
+
+#include "d3d_utility.h"
+
+#include <string.h>
+#include <SDL2/SDL.h>
+
+// Globals
+
+IDirect3DDevice9* Device = 0;
+const int Width = 640;
+const int Height = 480;
+const int SECOND = 1000;
+
+IDirect3DVertexBuffer9* Quad = 0;
+IDirect3DTexture9*      Tex  = 0;
+
+// Classes and Structures
+
+struct Vertex
+{
+	Vertex() {}
+	Vertex(
+		float x, float y, float z,
+		float nx, float ny, float nz,
+		float u, float v)
+	{
+		_x = x;  _y = y;  _z = z;
+		_nx = nx; _ny = ny; _nz = nz;
+		_u = u;  _v = v;
+	}
+	float _x, _y, _z;
+	float _nx, _ny, _nz;
+	float _u, _v; // texture coordinates
+
+	static const DWORD FVF;
+};
+const DWORD Vertex::FVF = D3DFVF_XYZ | D3DFVF_NORMAL | D3DFVF_TEX1;
+
+// Additional math functions
+
+static inline D3DMATRIX* MatrixIdentity(D3DMATRIX* pout)
+{
+	if (!pout) return nullptr;
+	pout->m[0][1] = 0.0f;
+	pout->m[0][2] = 0.0f;
+	pout->m[0][3] = 0.0f;
+	pout->m[1][0] = 0.0f;
+	pout->m[1][2] = 0.0f;
+	pout->m[1][3] = 0.0f;
+	pout->m[2][0] = 0.0f;
+	pout->m[2][1] = 0.0f;
+	pout->m[2][3] = 0.0f;
+	pout->m[3][0] = 0.0f;
+	pout->m[3][1] = 0.0f;
+	pout->m[3][2] = 0.0f;
+	pout->m[0][0] = 1.0f;
+	pout->m[1][1] = 1.0f;
+	pout->m[2][2] = 1.0f;
+	pout->m[3][3] = 1.0f;
+	return pout;
+}
+
+D3DMATRIX* MatrixPerspectiveFovLH(D3DMATRIX* pout, float fovy, float aspect, float zn, float zf)
+{
+	MatrixIdentity(pout);
+	pout->m[0][0] = 1.0f / (aspect * tanf(fovy / 2.0f));
+	pout->m[1][1] = 1.0f / tanf(fovy / 2.0f);
+	pout->m[2][2] = zf / (zf - zn);
+	pout->m[2][3] = 1.0f;
+	pout->m[3][2] = (zf * zn) / (zn - zf);
+	pout->m[3][3] = 0.0f;
+	return pout;
+}
+
+// Framework Functions
+
+bool Setup()
+{
+	// Create the vertex buffer.
+
+	Device->CreateVertexBuffer(
+		6 * sizeof(Vertex), // size in bytes
+		D3DUSAGE_WRITEONLY, // flags
+		Vertex::FVF,        // vertex format
+		D3DPOOL_MANAGED,    // managed memory pool
+		&Quad,              // return create vertex buffer
+		0);                 // not used - set to 0
+
+	// Fill the buffers with the triangle data.
+
+	Vertex* v;
+	Quad->Lock(0, 0, (void**)&v, 0);
+
+	// quad built from two triangles, note texture coordinates:
+	v[0] = Vertex(-1.0f, -1.0f, 1.25f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f);
+	v[1] = Vertex(-1.0f, 1.0f, 1.25f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f);
+	v[2] = Vertex(1.0f, 1.0f, 1.25f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f);
+
+	v[3] = Vertex(-1.0f, -1.0f, 1.25f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f);
+	v[4] = Vertex(1.0f, 1.0f, 1.25f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f);
+	v[5] = Vertex(1.0f, -1.0f, 1.25f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f);
+
+	Quad->Unlock();
+
+	//
+	// Create the texture and set filters.
+	//
+
+	D3DXCreateTextureFromFile(
+		Device,
+		"textures/cursor.dds",
+		&Tex);
+
+	Device->SetTexture(0, Tex);
+
+	Device->SetSamplerState(0, D3DSAMP_MAGFILTER, D3DTEXF_LINEAR);
+	Device->SetSamplerState(0, D3DSAMP_MINFILTER, D3DTEXF_LINEAR);
+	Device->SetSamplerState(0, D3DSAMP_MIPFILTER, D3DTEXF_POINT);
+
+	//
+	// Don't use lighting for this sample.
+	//
+	Device->SetRenderState(D3DRS_LIGHTING, false);
+
+	// Set the projection matrix.
+
+	D3DMATRIX proj;
+	MatrixPerspectiveFovLH(
+		&proj,                        // result
+		M_PI * 0.5f,                  // 90 - degrees
+		(float)Width / (float)Height, // aspect ratio
+		1.0f,                         // near plane
+		1000.0f);                     // far plane
+	Device->SetTransform(D3DTS_PROJECTION, &proj);
+
+	return true;
+}
+
+void Cleanup()
+{
+	d3d::Release<IDirect3DVertexBuffer9*>(Quad);
+	d3d::Release<IDirect3DTexture9*>(Tex);
+}
+
+void ShowPrimitive()
+{
+	if (Device)
+	{
+		Device->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 0xffffffff, 1.0f, 0);
+		Device->BeginScene();
+
+		Device->SetStreamSource(0, Quad, 0, sizeof(Vertex));
+		Device->SetFVF(Vertex::FVF);
+
+		// Draw one triangle.
+		Device->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 2);
+
+		Device->EndScene();
+		Device->Present(0, 0, 0, 0);
+	}
+}
+
+// init ... The init function, it calls the SDL init function.
+int initSDL() {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		return -1;
+	}
+
+	return 0;
+}
+
+// createWindowContext ... Creating the window for later use in rendering and stuff.
+SDL_Window* createWindowContext(std::string title) {
+	//Declaring the variable the return later.
+	SDL_Window* Window = NULL;
+
+	uint32_t flags;
+#if defined(_WIN32) || defined(USE_NINE)
+	flags = SDL_WINDOW_OPENGL;
+#else // for DXVK Native
+	flags = SDL_WINDOW_VULKAN;
+#endif
+
+	//Creating the window and passing that reference to the previously declared variable.
+	Window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, Width, Height, flags);
+
+	//Returning the newly creted Window context.
+	return Window;
+}
+
+// main ... The main function, right now it just calls the initialization of SDL.
+int main(int argc, char* argv[]) {
+	//Calling the SDL init stuff.
+	initSDL();
+
+	//Creating the context for SDL2.
+	SDL_Window* Window = createWindowContext("Hello Texture!");
+
+	if (!d3d::InitD3D(Window,
+		Width, Height, true, D3DDEVTYPE_HAL, &Device))
+	{
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "InitD3D() - FAILED", nullptr);
+		return 0;
+	}
+
+	if (!Setup())
+	{
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Setup() - FAILED", nullptr);
+		return 0;
+	}
+
+	bool running = true;
+	while (running)
+	{
+		SDL_Event ev;
+		while (SDL_PollEvent(&ev))
+		{
+			if ((SDL_QUIT == ev.type) ||
+				(SDL_KEYDOWN == ev.type && SDL_SCANCODE_ESCAPE == ev.key.keysym.scancode))
+			{
+				running = false;
+				break;
+			}
+		}
+		ShowPrimitive();
+	}
+
+	//Cleaning up everything.
+	Cleanup();
+	Device->Release();
+	SDL_Quit();
+
+	return 0;
+}
