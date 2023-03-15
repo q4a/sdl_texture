fa569ac72e0b7858a6c4200262e0aba8ead2e69c
Aleksey Komarov <q4arus@ya.ru>
2023-03-16 00:49:55 +0300

    add src for sdl_d3d9_cube

---
+++ sdl_d3d9_cube/src/d3d_utility.h
@@ -0,0 +1,58 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// File: d3dUtility.h
+//
+// Author: Frank Luna (C) All Rights Reserved
+//
+// System: AMD Athlon 1800+ XP, 512 DDR, Geforce 3, Windows XP, MSVC++ 7.0
+//
+// Desc: Provides utility functions for simplifying common tasks.
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __d3d_utility__
+#define __d3d_utility__
+
+#include <d3d9.h>
+#include <d3dx9.h>
+#include <SDL2/SDL.h>
+#include <string>
+
+namespace d3d
+{
+	void* OSHandle(SDL_Window* Window);
+	bool InitD3D(
+		SDL_Window* Window,        // [in] SDL Window handler
+		int width, int height,     // [in] Backbuffer dimensions.
+		bool windowed,             // [in] Windowed (true)or full screen (false).
+		D3DDEVTYPE deviceType,     // [in] HAL or REF
+		IDirect3DDevice9** device);// [out]The created device.
+
+	template<class T> void Release(T t)
+	{
+		if( t )
+		{
+			t->Release();
+			t = 0;
+		}
+	}
+
+	template<class T> void Delete(T t)
+	{
+		if( t )
+		{
+			delete t;
+			t = 0;
+		}
+	}
+
+	const D3DXCOLOR      WHITE( D3DCOLOR_XRGB(255, 255, 255) );
+	const D3DXCOLOR      BLACK( D3DCOLOR_XRGB(  0,   0,   0) );
+
+	// Materials
+
+	D3DMATERIAL9 InitMtrl(D3DXCOLOR a, D3DXCOLOR d, D3DXCOLOR s, D3DXCOLOR e, float p);
+	const D3DMATERIAL9 WHITE_MTRL  = InitMtrl(WHITE, WHITE, WHITE, BLACK, 2.0f);
+}
+
+#endif // __d3d_utility__
---
+++ sdl_d3d9_cube/src/sdl_d3d9_cube.cpp
@@ -0,0 +1,285 @@
+
+#include "d3d_utility.h"
+#include "cube.h"
+#include "vertex.h"
+
+#include <string.h>
+#include <SDL2/SDL.h>
+
+// Globals
+
+IDirect3DDevice9* Device = 0;
+const int Width = 640;
+const int Height = 480;
+
+Cube*              Box = 0;
+IDirect3DTexture9* Tex = 0;
+
+// Additional math functions
+
+static inline D3DMATRIX* MatrixIdentity(D3DMATRIX* pout)
+{
+	if (!pout) return nullptr;
+	pout->m[0][1] = 0.0f;
+	pout->m[0][2] = 0.0f;
+	pout->m[0][3] = 0.0f;
+	pout->m[1][0] = 0.0f;
+	pout->m[1][2] = 0.0f;
+	pout->m[1][3] = 0.0f;
+	pout->m[2][0] = 0.0f;
+	pout->m[2][1] = 0.0f;
+	pout->m[2][3] = 0.0f;
+	pout->m[3][0] = 0.0f;
+	pout->m[3][1] = 0.0f;
+	pout->m[3][2] = 0.0f;
+	pout->m[0][0] = 1.0f;
+	pout->m[1][1] = 1.0f;
+	pout->m[2][2] = 1.0f;
+	pout->m[3][3] = 1.0f;
+	return pout;
+}
+
+D3DMATRIX* MatrixPerspectiveFovLH(D3DMATRIX* pout, float fovy, float aspect, float zn, float zf)
+{
+	MatrixIdentity(pout);
+	pout->m[0][0] = 1.0f / (aspect * tanf(fovy / 2.0f));
+	pout->m[1][1] = 1.0f / tanf(fovy / 2.0f);
+	pout->m[2][2] = zf / (zf - zn);
+	pout->m[2][3] = 1.0f;
+	pout->m[3][2] = (zf * zn) / (zn - zf);
+	pout->m[3][3] = 0.0f;
+	return pout;
+}
+
+// Some d3dx9 functions
+
+#ifdef _WIN32
+#include <d3dx9.h>
+#else
+#include <unordered_map>
+#include <gli/gli.hpp>
+
+const std::unordered_map<gli::format, D3DFORMAT> gli_format_map{
+	{ gli::FORMAT_RGBA_DXT5_UNORM_BLOCK16, D3DFMT_DXT5 },
+	{ gli::FORMAT_RGBA_DXT5_SRGB_BLOCK16, D3DFMT_DXT5 },
+};
+#endif
+
+HRESULT CreateTextureFromFile(
+	IDirect3DDevice9 *device,
+	const char *srcfile,
+	IDirect3DTexture9 **texture)
+{
+#ifdef _WIN32
+	return D3DXCreateTextureFromFile(device, srcfile, texture);
+#else
+
+	gli::texture tex = gli::load(srcfile);
+	const auto dimensions = tex.extent();
+	HRESULT hr;
+
+	hr = Device->CreateTexture(dimensions.x, dimensions.y, 1, 0, gli_format_map.at(tex.format()), D3DPOOL_MANAGED, texture, nullptr);
+	if (FAILED(hr))
+	{
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "LockRect failed", nullptr);
+		return hr;
+	}
+
+	D3DLOCKED_RECT rect;
+	hr = (*texture)->LockRect( 0, &rect, 0, D3DLOCK_DISCARD );
+	if (FAILED(hr))
+	{
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "LockRect failed", nullptr);
+		return hr;
+	}
+	char* dest = static_cast<char*>(rect.pBits);
+	memcpy(dest, tex.data(), tex.size());
+	hr = (*texture)->UnlockRect(0);
+
+	return hr;
+#endif
+}
+
+// Framework Functions
+
+bool Setup()
+{
+	// Create the cube.
+
+	Box = new Cube(Device);
+
+	// Set a directional light.
+
+	D3DLIGHT9 light;
+	::ZeroMemory(&light, sizeof(light));
+	light.Type      = D3DLIGHT_DIRECTIONAL;
+	light.Ambient   = D3DXCOLOR(0.8f, 0.8f, 0.8f, 1.0f);
+	light.Diffuse   = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+	light.Specular  = D3DXCOLOR(0.2f, 0.2f, 0.2f, 1.0f);
+	light.Direction = D3DXVECTOR3(1.0f, -1.0f, 0.0f);
+	Device->SetLight(0, &light);
+	Device->LightEnable(0, true);
+
+	Device->SetRenderState(D3DRS_NORMALIZENORMALS, true);
+	Device->SetRenderState(D3DRS_SPECULARENABLE, true);
+
+	// Create texture.
+
+	CreateTextureFromFile(
+		Device,
+		"textures/cursor.dds",
+		&Tex);
+
+	// Set Texture Filter States.
+
+	Device->SetSamplerState(0, D3DSAMP_MAGFILTER, D3DTEXF_LINEAR);
+	Device->SetSamplerState(0, D3DSAMP_MINFILTER, D3DTEXF_LINEAR);
+	Device->SetSamplerState(0, D3DSAMP_MIPFILTER, D3DTEXF_POINT);
+
+	// Set the projection matrix.
+
+	D3DMATRIX proj;
+	MatrixPerspectiveFovLH(
+		&proj,                        // result
+		M_PI * 0.5f,                  // 90 - degrees
+		(float)Width / (float)Height, // aspect ratio
+		1.0f,                         // near plane
+		1000.0f);                     // far plane
+	Device->SetTransform(D3DTS_PROJECTION, &proj);
+
+	return true;
+}
+
+void Cleanup()
+{
+	d3d::Delete<Cube*>(Box);
+	d3d::Release<IDirect3DTexture9*>(Tex);
+}
+
+void ShowPrimitive(SDL_Event ev, float deltaTime)
+{
+	if (Device)
+	{
+
+		// Update the scene: update camera position.
+
+		static float angle  = (3.0f * D3DX_PI) / 2.0f;
+		static float height = 2.0f;
+
+		if(ev.type == SDL_KEYDOWN && ev.key.keysym.scancode == SDL_SCANCODE_A)
+			angle -= 0.5f * deltaTime;
+
+		if(ev.type == SDL_KEYDOWN && ev.key.keysym.scancode == SDL_SCANCODE_D)
+			angle += 0.5f * deltaTime;
+
+		if(ev.type == SDL_KEYDOWN && ev.key.keysym.scancode == SDL_SCANCODE_W)
+			height += 5.0f * deltaTime;
+
+		if(ev.type == SDL_KEYDOWN && ev.key.keysym.scancode == SDL_SCANCODE_S)
+			height -= 5.0f * deltaTime;
+
+		D3DXVECTOR3 position( cosf(angle) * 3.0f, height, sinf(angle) * 3.0f );
+		D3DXVECTOR3 target(0.0f, 0.0f, 0.0f);
+		D3DXVECTOR3 up(0.0f, 1.0f, 0.0f);
+		D3DXMATRIX V;
+		D3DXMatrixLookAtLH(&V, &position, &target, &up);
+
+		Device->SetTransform(D3DTS_VIEW, &V);
+
+		// Draw the scene:
+
+		Device->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 0xffffffff, 1.0f, 0);
+		Device->BeginScene();
+
+		Device->SetMaterial(&d3d::WHITE_MTRL);
+		Device->SetTexture(0, Tex);
+
+		Box->draw(0, 0, 0);
+
+		Device->EndScene();
+		Device->Present(0, 0, 0, 0);
+	}
+}
+
+// init ... The init function, it calls the SDL init function.
+int initSDL() {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		return -1;
+	}
+
+	return 0;
+}
+
+// createWindowContext ... Creating the window for later use in rendering and stuff.
+SDL_Window* createWindowContext(std::string title) {
+	//Declaring the variable the return later.
+	SDL_Window* Window = NULL;
+
+	uint32_t flags;
+#if defined(_WIN32) || defined(USE_NINE)
+	flags = SDL_WINDOW_OPENGL;
+#else // for DXVK Native
+	flags = SDL_WINDOW_VULKAN;
+#endif
+
+	//Creating the window and passing that reference to the previously declared variable.
+	Window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, Width, Height, flags);
+
+	//Returning the newly creted Window context.
+	return Window;
+}
+
+// main ... The main function, right now it just calls the initialization of SDL.
+int main(int argc, char* argv[]) {
+	//Calling the SDL init stuff.
+	initSDL();
+
+	//Creating the context for SDL2.
+	SDL_Window* Window = createWindowContext("Hello Texture!");
+
+	if (!d3d::InitD3D(Window,
+		Width, Height, true, D3DDEVTYPE_HAL, &Device))
+	{
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "InitD3D() - FAILED", nullptr);
+		return 0;
+	}
+
+	if (!Setup())
+	{
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Setup() - FAILED", nullptr);
+		return 0;
+	}
+
+	float deltaTime = 0.0f;
+	float lastFrame = 0.0f;//SDL_GetTicks() / 1000.0f;
+	//double deltaTime = 0;
+	//uint64_t lastFrame = SDL_GetPerformanceCounter();
+	bool running = true;
+	while (running)
+	{
+		SDL_Event ev;
+		while (SDL_PollEvent(&ev))
+		{
+			float currentFrame = SDL_GetTicks() / 1000.0f;
+			deltaTime = currentFrame - lastFrame;
+			lastFrame = currentFrame;
+			//float currentFrame = SDL_GetPerformanceCounter();
+			//deltaTime = (double)((currentFrame - lastFrame) / (double)SDL_GetPerformanceFrequency());
+			//lastFrame = currentFrame;
+			if ((ev.type == SDL_QUIT) ||
+				(ev.type == SDL_KEYDOWN && ev.key.keysym.scancode == SDL_SCANCODE_ESCAPE))
+			{
+				running = false;
+				break;
+			}
+			ShowPrimitive(ev, deltaTime);
+		}
+	}
+
+	//Cleaning up everything.
+	Cleanup();
+	Device->Release();
+	SDL_Quit();
+
+	return 0;
+}
